c306452118be02e47e252227b0384a1f3f99174f
dzp62442 <dzp62442@163.com>
2023-05-06 17:27:29 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include "sophus/se3.hpp"
+
+int main ( int argc, char** argv )
+{
+    std::cout << "Hello, world!" << std::endl;
+}
