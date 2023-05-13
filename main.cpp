fbec8099d05d1d1f0dd790cf0abb910ca572a7c5
dzp62442 <dzp62442@163.com>
2023-05-13 08:48:38 +0800

    Submit

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main ( int argc, char** argv )
     calibrator.calib();
     cv::Mat K = calibrator.K.clone();
     BAOptimizer optimizer(K);
-    int gn_ms = 0, lm_ms = 0, gn_ceres_ms = 0, lm_ceres_ms = 0;  // 各种算法总耗时
+    int gn_ms = 0, lm_ms = 0;  // 各种算法总耗时
 
     // 对每张图像求解PnP问题
     for (int n=0; n<calibrator.num_images; n++){
@@ -37,8 +37,8 @@ int main ( int argc, char** argv )
         Eigen::Quaterniond q = Eigen::Quaterniond::UnitRandom();
         q.normalize();
         Eigen::Vector3d t = Eigen::Vector3d::Random();
-        // Sophus::SE3d pose_gn(q, t), pose_lm(q, t), pose_gn_ceres(q, t), pose_lm_ceres(q, t);
-        Sophus::SE3d pose_gn(rot, tvec_eigen), pose_lm(rot, tvec_eigen), pose_gn_ceres(rot, tvec_eigen), pose_lm_ceres(rot, tvec_eigen);
+        Sophus::SE3d pose_gn(q, t), pose_lm(q, t), pose_gn_ceres(q, t), pose_lm_ceres(q, t);
+        // Sophus::SE3d pose_gn(rot, tvec_eigen), pose_lm(rot, tvec_eigen), pose_gn_ceres(rot, tvec_eigen), pose_lm_ceres(rot, tvec_eigen);
 
         Timer timer;  // 初始化计时器
 
@@ -52,30 +52,9 @@ int main ( int argc, char** argv )
         lm_ms += timer.update("Levenberg-Marquardt");
         // std::cout << "Pose by Levenberg-Marquardt: \n" << pose_lm.matrix() << std::endl;
 
-        // Ceres 的高斯牛顿法优化
-        optimizer.CeresSolver(points_3d_eigen, points_2d_eigen, pose_gn_ceres);
-        gn_ceres_ms += timer.update("Ceres Gauss-Newton");
-        // std::cout << "Pose by Ceres Gauss-Newton: \n" << pose_gn_ceres.matrix() << std::endl;
-
-        // Ceres 的 LM 算法优化
-        optimizer.CeresSolver(points_3d_eigen, points_2d_eigen, pose_lm_ceres, 1);
-        lm_ceres_ms += timer.update("Ceres Levenberg-Marquardt");
-        // std::cout << "Pose by Ceres Levenberg-Marquardt: \n" << pose_lm_ceres.matrix() << std::endl;
-
-        // // DLT求解PnP问题
-        // Sophus::SE3d pose_dlt;
-        // optimizer.DLT(points_3d_eigen, points_2d_eigen, pose_dlt);
-        // std::cout << "Pose of DLT: \n" << pose_dlt.matrix() << std::endl;
-
-        // // 以DLT求解结果作为BA位姿初值
-        // optimizer.LevenbergMarquardt(points_3d_eigen, points_2d_eigen, pose_dlt);
-        // std::cout << "Pose by Gauss-Newton based on DLT: \n" << pose_dlt.matrix() << std::endl;
-
     }
 
     std::cout << "Gauss-Newton total time: " << gn_ms << " ms\n";
     std::cout << "Levenberg-Marquardt total time: " << lm_ms << " ms\n";
-    std::cout << "Ceres Gauss-Newton total time: " << gn_ceres_ms << " ms\n";
-    std::cout << "Ceres Levenberg-Marquardt total time: " << lm_ceres_ms << " ms\n";
 
 }
